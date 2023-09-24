ac2e461faacd367b5c3c6cc334a9fab22b4aefce
Eva Kemp <eva.j.kemp@gmail.com>
2023-09-24 14:52:34 -0700

    update index, add bootstrap

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { ReactNode } from "react";
+
+// interface Props {
+//   children: ReactNode;
+// }
+// const Header = ({ children }: Props) => {
+//   return <div className="alert alert-primary">{children}</div>;
+// };
+
+function Header() {
+  return <div>Welcome!</div>;
+}
+
+export default Header;
