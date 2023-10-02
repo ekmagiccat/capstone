86de6b9fded02bc23cd08068a55183d572738bf2
Eva Kemp <eva.j.kemp@gmail.com>
2023-10-02 15:32:09 -0700

    fix file path for images

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import Header from "./Header";
 import Instructions from "./Instructions";
 import GamePlay from "./GamePlay";
+import CoffeePot from "./CoffeePot";
 
 const App = () => {
   return (
     <div className="app">
       <Header />
+      <CoffeePot />
       <Instructions />
       <GamePlay />
     </div>
--- src/components/CoffeePot.tsx
+++ src/components/CoffeePot.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import chemex from "../images/chemex.jpg";
 
 const CoffeePot = () => {
-  return <div>CoffeePot</div>;
+  return (
+    <div>
+      <p>CoffeePot</p>
+      <img src={chemex} alt="pour over coffee" className="coffee" />
+    </div>
+  );
 };
 
 export default CoffeePot;
