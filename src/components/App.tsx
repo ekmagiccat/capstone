35617625ab5edc2e3d0d7d0677051dbaa0ec5901
Eva Kemp <eva.j.kemp@gmail.com>
2023-10-01 17:04:58 -0700

    update game play

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Header from "./Header";
 import Instructions from "./Instructions";
+import GamePlay from "./GamePlay";
+import { useState } from "react";
 
 const App = () => {
+ 
+
   return (
     <div>
       <Header />
       <Instructions />
-      
+      <GamePlay />
     </div>
   );
 };
--- src/components/GamePlay.tsx
+++ src/components/GamePlay.tsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
-const GameControl = () => {
+const GamePlay = () => {
+  const [waterValue, setWaterValue] = useState(0);
+
+  const handleAddWater = () => {
+    setWaterValue(waterValue + 5);
+  };
+
   return (
     <div>
-      <h2>Play</h2>
+      <p className="water">{waterValue}</p>
+      <button type="button" onClick={handleAddWater}>
+        Add Water
+      </button>
     </div>
   );
 };
 
-export default GameControl;
+export default GamePlay;
