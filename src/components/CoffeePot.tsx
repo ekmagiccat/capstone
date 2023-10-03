db302ee2987430586ccaec5c026e10bfd3b71c84
Eva Kemp <eva.j.kemp@gmail.com>
2023-10-03 00:00:17 -0700

    wip

--- src/components/CoffeePot.tsx
+++ src/components/CoffeePot.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import pourOver from "../images/pour-over.png";
+import chemex from "../images/chemex.png";
+import waterPour from "../images/waterPour.png";
 import { motion } from "framer-motion";
 
 const CoffeePot = () => {
@@ -7,13 +8,14 @@ const CoffeePot = () => {
 
   return (
     <motion.div
-      animate={{ rotate: rotate ? 360 : 0 }}
+      animate={{ rotate: rotate ? 360 : 20 }}
       transition={{ type: "tween", duration: 1 }}
       onClick={() => {
         setRotate(!rotate);
       }}
     >
-      <img src={pourOver} alt="pour over coffee" className="coffee" />
+      <img src={waterPour} alt="kettle pouring water" />
+      <img src={chemex} alt="pour over coffee" className="chemex" />
     </motion.div>
   );
 };
