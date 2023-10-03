853158c12c7d9aedcfb3d6269891829e55348c78
Eva Kemp <eva.j.kemp@gmail.com>
2023-10-02 23:34:48 -0700

    test framer motion

--- src/components/CoffeePot.tsx
+++ src/components/CoffeePot.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import pourOver from "../images/pour-over.png";
+import { motion } from "framer-motion";
 
 const CoffeePot = () => {
+  const [rotate, setRotate] = React.useState(false);
+
   return (
-    <div>
-      <p>Coffee Pot</p>
+    <motion.div
+      animate={{ rotate: rotate ? 360 : 0 }}
+      transition={{ type: "tween", duration: 1 }}
+      onClick={() => {
+        setRotate(!rotate);
+      }}
+    >
       <img src={pourOver} alt="pour over coffee" className="coffee" />
-    </div>
+    </motion.div>
   );
 };
 
